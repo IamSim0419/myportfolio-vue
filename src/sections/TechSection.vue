a9466cc0d8e78a53c89671bda048d5edd3e3d5fe
Simreich Somogod <simreich1989@gmail.com>
2025-08-25 07:27:42 +0800

    fix icon issue 1

--- src/sections/TechSection.vue
+++ src/sections/TechSection.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 // Tech images for Tech Section
-import FigmaIcon from '@/assets/icons/figma.svg'
-import GsapIcon from '@/assets/icons/gsap.svg'
-import VueIcon from '@/assets/icons/vue.svg'
-import TypescriptIcon from '@/assets/icons/typescript.svg'
+import FigmaIcon from '/figma.svg'
+import GsapIcon from '/gsap.svg'
+import VueIcon from '/vue.svg'
+import TypescriptIcon from '/typescript.svg'
 
 const techStack = [
   { name: 'Typescript', icon: TypescriptIcon },
